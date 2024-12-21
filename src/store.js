6350cdda578d4e5b66d6de8f713e2291a3e117d4
KhalilArfaoui41 <khalil.rayen.k@gmail.com>
2024-12-21 16:31:34 +0100

    update path

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import cartReducer from './features/cartSlice'
+import cartReducer from './features/Cart/cartSlice'
 import userReducer from './features/user/userSlice'
 
 export const store = configureStore({
